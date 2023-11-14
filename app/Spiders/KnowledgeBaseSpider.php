f90734fd957c7ba526564300fb33c69d5fb648e6
199ocero <199ocero@gmail.com>
2023-11-14 20:37:20 +0800

    added knowledge base spider

---
+++ app/Spiders/KnowledgeBaseSpider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Spiders;
+
+use Generator;
+use RoachPHP\Downloader\Middleware\RequestDeduplicationMiddleware;
+use RoachPHP\Extensions\LoggerExtension;
+use RoachPHP\Extensions\StatsCollectorExtension;
+use RoachPHP\Http\Response;
+use RoachPHP\Spider\BasicSpider;
+use RoachPHP\Spider\ParseResult;
+
+class KnowledgeBaseSpider extends BasicSpider
+{
+    public array $startUrls = [
+        'https://roach-php.dev/docs/spiders',
+        'https://roach-php.dev/docs/introduction'
+    ];
+
+    public array $downloaderMiddleware = [
+        RequestDeduplicationMiddleware::class,
+    ];
+
+    public array $spiderMiddleware = [
+        //
+    ];
+
+    public array $itemProcessors = [
+        //
+    ];
+
+    public array $extensions = [
+        LoggerExtension::class,
+        StatsCollectorExtension::class,
+    ];
+
+    public int $concurrency = 2;
+
+    public int $requestDelay = 1;
+
+    /**
+     * @return Generator<ParseResult>
+     */
+    public function parse(Response $response): Generator
+    {
+        yield $this->item([
+            'response' => $response->filter('body')->text()
+        ]);
+    }
+}
