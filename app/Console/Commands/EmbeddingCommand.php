15cf8bb2afff7401db79961892b7017ad45b7956
199ocero <199ocero@gmail.com>
2023-11-15 12:55:50 +0800

    added embedding command

---
+++ app/Console/Commands/EmbeddingCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class EmbeddingCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:embedding';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command will be used to generate embedding';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
