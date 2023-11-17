e360e60c2a65e36074fed719783636f7a5008690
199ocero <199ocero@gmail.com>
2023-11-17 20:01:17 +0800

    added ask question

--- app/Services/OpenAIAgent.php
+++ app/Services/OpenAIAgent.php
@@ -7,30 +7,52 @@ use OpenAI;
 class OpenAIAgent
 {
     protected $client;
-    protected $text;
 
-    public function __construct(string $text)
+    public function __construct()
     {
         if (config('openai.api_key') === null) {
             throw new \Exception('OpenAI API key is not set.');
         }
 
         $this->client = OpenAI::client(config('openai.api_key'));
-        $this->text = $text;
     }
 
-    public static function make(string $text): self
+    public static function make(): self
     {
-        return new self($text);
+        return new self();
     }
 
-    public function embeddings(): array
+    public function embeddings(string $text): array
     {
         $response = $this->client->embeddings()->create([
             'model' => 'text-embedding-ada-002',
-            'input' => $this->text,
+            'input' => $text,
         ]);
 
         return $response->toArray()['data'][0]['embedding'];
+    }
+
+    public function askQuestion(string $context, string $question): string
+    {
+        $system_template = "
+            Use the following pieces of context to answer the users question.
+            If you don't know the answer, just say that you don't know, don't try to make up an answer.
+            ----------------
+            {context}
+        ";
+
+        $system_prompt = str_replace("{context}", $context, $system_template);
+
+        $response = $this->client->chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'max_tokens' => 200,
+            'temperature' => 0.8,
+            'messages' => [
+                ['role' => 'system', 'content' => $system_prompt],
+                ['role' => 'user', 'content' => $question],
+            ],
+        ]);
+
+        return $response->toArray()['choices'][0]['message']['content'];
     }
 }
